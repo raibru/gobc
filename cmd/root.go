52709d5ef324d3f28d5589a5c3e9371cd9f09e40
rbr <rbr@arcor.de>
2020-04-07 21:32:04 +0200

    FEAT(base): overwork lead-zero decription

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&bc.Dec, "dec", "d", "", "convert string number from decimal.\nRead from pipe use '.' as parameter")
 	rootCmd.PersistentFlags().StringVarP(&bc.Oct, "oct", "o", "", "convert string number from octal.\nRead from pipe use '.' as parameter")
 	rootCmd.PersistentFlags().StringVarP(&bc.Bin, "bin", "b", "", "convert string number from binary.\nRead from pipe use '.' as parameter")
-	rootCmd.PersistentFlags().StringVarP(&bc.LeadZero, "lead-zero", "", "", "Print data with leading zero defined by string e.g 'x:4,d:2,o:3,b:8'\n  hex: x:4 - 4 leadings 0\n  dec: d:2 - 2 leadings 0\n  oct: o:3 - 3 leadings 0\n  bin: b:tttt8 - 8 leadings 0\nseparated by ','")
+	rootCmd.PersistentFlags().StringVarP(&bc.LeadZero, "lead-zero", "", "", "Print data with leading zeros\ndefined by a string and separated by ','\ne.g 'x:4,d:2,o:3,b:8'\n  hex: x:4 -> 4 leading zeros\n  dec: d:2 -> 2 leading zeros\n  oct: o:3 -> 3 leading zeros\n  bin: b:8 -> 8 leading zeros")
 	rootCmd.PersistentFlags().BoolVarP(&prtPrefix, "prefix", "p", false, "display converted values with base prefix")
 	rootCmd.PersistentFlags().BoolVarP(&prtVersion, "version", "v", false, "display gobc version")
 }
